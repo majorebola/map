ed2718b0b9a4adb74f4572c4ae82aced51ea9db9
majorebola <edoardo.davini@gmail.com>
2016-04-19 15:38:56 +0200

    added distance calculator

--- js/main.js
+++ js/main.js
@@ -102,11 +102,59 @@ var createPath = function(index) {
             for (var i = 0; i < length; i++) {
                 tpath.push(inpath[i]);
             }
-            debugger;
             poly.setPath(tpath);
             poly.setMap(map);
+            addTravelDatas(result.routes[0].legs[0]);
         }
     });
+};
+
+var addTravelDatas = function(leg) {
+    var totalDistanceElement = document.getElementById('total-distance');
+    var totalDurationElement = document.getElementById('total-duration');
+    var totalDistanceUnitElement = document.getElementById('total-distance-unit');
+    var totalDurationUnitElement = document.getElementById('total-duration-unit');
+
+    var totalDistance = parseInt(totalDistanceElement.innerHTML);
+    var totalDuration = parseInt(totalDurationElement.innerHTML);
+    var totalDistanceUnit = totalDistanceUnitElement.innerHTML;
+    var totalDurationUnit = totalDurationUnitElement.innerHTML;
+
+    var newDistance = leg.distance.value;
+    var newDuration = leg.duration.value;
+
+    if (totalDistanceUnit == 'km') {
+        newDistance /= 1000;
+    }
+    switch (totalDurationUnit) {
+        case 'hours':
+            newDuration /= 3600;
+            break;
+        case 'mins':
+            newDuration /= 60;
+            break;
+    }
+
+    totalDistance += newDistance;
+    totalDuration += newDuration;
+
+    if(totalDistance > 1000) {
+        totalDistance /= 1000;
+        totalDistanceUnit = "km";
+    }
+    if(totalDuration > 60 && totalDurationUnit == 'secs') {
+        totalDuration /= 60;
+        totalDurationUnit = "mins";
+    }
+    if(totalDuration > 60 && totalDurationUnit == 'mins') {
+        totalDuration /= 60;
+        totalDurationUnit = "hours";
+    }
+
+    totalDistanceElement.innerHTML = totalDistance;
+    totalDurationElement.innerHTML = totalDuration;
+    totalDistanceUnitElement.innerHTML = totalDistanceUnit;
+    totalDurationUnitElement.innerHTML = totalDurationUnit;
 };
 
 var addPoint = function() {
