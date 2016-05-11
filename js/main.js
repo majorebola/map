75617a628d4dec8fa238d108f2f20ad137218fff
edoardo.davini <edoardo.davini@gmail.com>
2016-05-12 00:08:07 +0200

    MapManager first stable version
    Complete MapManager with added core function: delete marker.
    Now everything is connected:
    input > searchBox > marker
    marker has previous and next marker and a path to the next marker.
    Everythings works.
    Calcualted Data should be fixed (remove doesn't remove data, but adds)
    But core feature is working

--- js/main.js
+++ js/main.js
@@ -5,26 +5,16 @@ var map;                    // google map
 var mapElement;             // map element where google map instanciates
 var toolbar;                // toolbar element where drawing searchboxes
 var listPlaces;             // searchBoxes container
-var searchBoxes = [];       // searchBoxes elements of google map
 
-var markers = [];           // markers elements of the google map
-var directionsService;      // directions Service for getting the path
-var path = {};              // path object (list) from google maps
-var poly;                   // polyline used for drawing the path
+var searchBoxesIndex = 0;
 
 var initMap = function() {
     console.log("initialization of everything");
     mapElement = document.getElementById('map');
 
-    directionsService = new google.maps.DirectionsService();
     map = new google.maps.Map(mapElement, {
         center: {lat: 45.05, lng: 7.65},
         zoom: 11
-    });
-    poly = new google.maps.Polyline({
-        strokeColor: '#DE4343',
-        strokeWeight: 3,
-        map: map
     });
 
     MapManager.init(map);
@@ -33,9 +23,9 @@ var initMap = function() {
     listPlaces = document.getElementById('list-places');
 };
 
-var createSearchBox = function(parent, index) {
+var createSearchBox = function(parent) {
 
-    var id = index;
+    var id = searchBoxesIndex++;
     var templateData = {
         id: id
     };
@@ -46,11 +36,9 @@ var createSearchBox = function(parent, index) {
         $(parent).append(html);
 
         var input = document.getElementById("sb-" + id);
+        input.searchBox = new google.maps.places.SearchBox(input);
 
-        searchBoxes[index] = new google.maps.places.SearchBox(input);
-        searchBoxes[index].index = index;
-
-        addSearchBoxListenerChanged(searchBoxes[index]);
+        addSearchBoxListenerChanged(input.searchBox);
     });
 };
 
@@ -58,7 +46,7 @@ var addSearchBoxListenerChanged = function(searchBox) {
     searchBox.addListener('places_changed', function() {
         var places = searchBox.getPlaces();
         if(places.length == 1) {
-            addMarkerAndCreatePath(places[0], searchBox.index);
+            addMarkerAndCreatePath(places[0], searchBox);
         } else if(places.length == 0){
             alert("No point found");
         } else {
@@ -67,117 +55,24 @@ var addSearchBoxListenerChanged = function(searchBox) {
     });
 };
 
-var addMarkerAndCreatePath = function(place) {
-    // create the marker
-    MapManager.addMarker(MapManager.createMarker(place));
-
-    // calculate path
-    if(markers.length > 1) {
-        createPath(index);
-    }
+var addMarkerAndCreatePath = function(place, searchBox) {
+    var marker = MapManager.createMarker(place);
+    marker.searchBox = searchBox;
+    searchBox.marker = marker;
+    MapManager.addMarker(marker);
 };
-
-
-
-/**
- * Calculate Path between Start and End. calls a callback with the result (if OK)
- * TODO: better handling of errors.
- * @param start
- * @param end
- * @param index
- * @param callback
- */
-var calculatePath = function(start, end, index, callback) {
-    console.log("PANIC MONSTER");
-    directionsService.route({
-        origin: start,
-        destination: end,
-        travelMode: google.maps.DirectionsTravelMode.DRIVING
-    }, function(result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            result.index = index;
-            callback(result);
-        } else {
-            console.log("error");
-        }
-    });
-};
-
-var buildPath = function(result) {
-    var innerPath = new google.maps.MVCArray();
-    for (var i = 0, len = result.routes[0].overview_path.length; i < len; i++) {
-        innerPath.push(result.routes[0].overview_path[i]);
-    }
-    return innerPath;
-};
-
-var createPath = function(index) {
-
-    var callback = function(result) {
-        path[index] = buildPath(result);
-
-        drawPath();
-        DataManager.addTravelDatas(result.routes[0].legs[0]);
-    };
-
-    calculatePath(markers[index-1].getPosition(), markers[index].getPosition(), index, callback);
-};
-
-var drawPath = function() {
-    var tpath = new google.maps.MVCArray();
-
-    for (var k in path) {
-        for (var i = 1; i < path[k].length; i++) {
-            tpath.push(path[k].getAt(i));
-        }
-    }
-    poly.setPath(tpath);
-    poly.setMap(map);
-};
-
 
 // (pseudo)listener
 var addPoint = function() {
-    // get the max index +1 for the new index
-    createSearchBox(listPlaces, getMaxSearchBoxIndex(searchBoxes)+1);
+    createSearchBox(listPlaces);
 };
 
-/**
- * calculate actual max index of the searchBoxes.
- * @param array
- * @returns {number}
- */
-var getMaxSearchBoxIndex = function(array) {
-    var max = -1;
-    for(var k in array) {
-        var i = array[k].index;
-        if (max < i) {
-            max = i;
-        }
-    }
-    return max;
-};
 var removePoint = function(element) {
-    var index = $(element).data('id');
-    $("#sbc-" + index).remove();
-    if (markers[index]) {
-        markers[index].setMap(null);
-        markers.splice(index,1);
-    }
-    debugger;
-
-    delete path[index];
-    delete path[index-1];
-
-    var callback = function(result) {
-        path[result.index] = buildPath(result);
-
-        drawPath();
-
-        addTravelDatas(result.routes[0].legs[0]);
-    };
-
-    calculatePath(markers[index-1].getPosition(), markers[index].getPosition(), index, callback);
+    //$(element).siblings('.search-box')[0].searchBox.marker.previousMarker.pathToNextMarker
+    var $element = $(element);
+    var searchBox = $element.siblings('.search-box')[0].searchBox;
+    MapManager.removeMarker(searchBox.marker);
+    $element.parent().remove();
 };
 
 
--- js/managers/MapManager.js
+++ js/managers/MapManager.js
@@ -5,6 +5,8 @@ var MapManager = (function() {
     var self = this;
     var markers;
     var map;
+    var directionsService;
+    var poly;
 
     var getLastMarker = function() {
         if(markers.length > 0) {
@@ -23,9 +25,60 @@ var MapManager = (function() {
         map.fitBounds(bounds);
     };
 
+    var buildPath = function(result) {
+        var innerPath = new google.maps.MVCArray();
+        for (var i = 0, len = result.routes[0].overview_path.length; i < len; i++) {
+            innerPath.push(result.routes[0].overview_path[i]);
+        }
+        return innerPath;
+    };
+
+    var drawPath = function() {
+        var tpath = new google.maps.MVCArray();
+
+        for (var k in markers) {
+            if (markers.hasOwnProperty(k) && markers[k].pathToNextMarker) {
+                for (var i = 0; i < markers[k].pathToNextMarker.length; i++) {
+                    tpath.push(markers[k].pathToNextMarker.getAt(i));
+                }
+            }
+        }
+        poly.setPath(tpath);
+        poly.setMap(map);
+    };
+
+    /**
+     * Private call to Google Maps Services.
+     * TODO: better error handling
+     * @param start
+     * @param end
+     * @param callback
+     */
+    var calculatePath = function(start, end, callback) {
+        directionsService.route({
+            origin: start,
+            destination: end,
+            travelMode: google.maps.DirectionsTravelMode.DRIVING
+        }, function(result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                // result.index = index;
+                callback(result);
+            } else {
+                console.log("error");
+            }
+        });
+    };
+
     self.init = function(inputMap) {
         markers = [];
         map = inputMap;
+
+        directionsService = new google.maps.DirectionsService();
+        poly = new google.maps.Polyline({
+            strokeColor: '#DE4343',
+            strokeWeight: 3,
+            map: map
+        });
     };
 
     // Not sure if useful, now for debug purpose only
@@ -33,6 +86,7 @@ var MapManager = (function() {
         return markers;
     };
 
+    // Not sure if useful, shoud be included in the addMarker function?
     self.createMarker = function(place) {
         return new google.maps.Marker({
             map: map,
@@ -50,8 +104,39 @@ var MapManager = (function() {
         }
         markers.push(marker);
         updateBounds();
+
+        if (markers.length > 1) {
+            var callback = function(result) {
+                previousMarker.pathToNextMarker = buildPath(result);
+                drawPath();
+                DataManager.addTravelDatas(result.routes[0].legs[0]);
+            };
+            calculatePath(previousMarker.getPosition(), marker.getPosition(), callback);
+        }
     };
 
+    self.removeMarker = function(marker) {
+        if(marker.previousMarker && marker.nextMarker) {
+            marker.previousMarker.nextMarker = marker.nextMarker;
+            marker.nextMarker.previousMarker = marker.previousMarker;
+            var callback = function(result) {
+                marker.previousMarker.pathToNextMarker = buildPath(result);
+                drawPath();
+                DataManager.addTravelDatas(result.routes[0].legs[0]);
+            };
+            calculatePath(marker.previousMarker.getPosition(), marker.nextMarker.getPosition(), callback);
+        }
+        markers.splice(getMarkerIndex(marker), 1);
+        marker.setMap(null);
+    };
+
+    var getMarkerIndex = function(marker) {
+        for (var m in markers) {
+            if (markers.hasOwnProperty(m) && marker == markers[m]) {
+                return m;
+            }
+        }
+    };
 
     return self;
 })();
